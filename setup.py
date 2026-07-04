22e72d50d1dfd5a19cd4f80fd3b1bba164e3e0ec
Komal <komalcheema@gmail.com>
2026-07-04 12:50:17 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version = '0.0.0',
+    author = 'Komaldeep kaur',
+    author_email = 'komalc0612@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
